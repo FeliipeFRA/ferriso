6d1b4925613f4ade41f03e792fb0c4c0e38e7de1
Felipe Ferreira <165099343+FeliipeFRA@users.noreply.github.com>
2025-08-25 16:46:19 -0300

    facts

--- index.php
+++ index.php
@@ -218,17 +218,19 @@
     <!-- Carousel End -->
 
 
-    <!-- Facts Start -->
+    <!-- Informacoes -->
     <div class="container-xxl py-5">
         <div class="container">
-            <div class="row g-4">
-                <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
+            <div class="row g-3">
+                <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
                     <div class="fact-item bg-light rounded text-center h-100 p-5">
-                        <i class="fa fa-certificate fa-4x text-primary mb-4"></i>
-                        <h5 class="mb-3">Years Experience</h5>
-                        <h1 class="display-5 mb-0" data-toggle="counter-up">1234</h1>
+                        <i class="fa fa-calendar fa-4x text-primary mb-4"></i>
+                        <h5 class="mb-3">Anos de Experiência</h5>
+                        <h1 class="display-5 mb-0" data-toggle="counter-up">26</h1>
                     </div>
                 </div>
+                <!-- ADICIONAR QUANDO EQUIPE FOR MAIOR
+
                 <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.3s">
                     <div class="fact-item bg-light rounded text-center h-100 p-5">
                         <i class="fa fa-users-cog fa-4x text-primary mb-4"></i>
@@ -236,24 +238,25 @@
                         <h1 class="display-5 mb-0" data-toggle="counter-up">1234</h1>
                     </div>
                 </div>
-                <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.5s">
+                -->
+                <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.3s">
                     <div class="fact-item bg-light rounded text-center h-100 p-5">
                         <i class="fa fa-users fa-4x text-primary mb-4"></i>
-                        <h5 class="mb-3">Satisfied Clients</h5>
-                        <h1 class="display-5 mb-0" data-toggle="counter-up">1234</h1>
+                        <h5 class="mb-3">Clientes Satisfeitos</h5>
+                        <h1 class="display-5 mb-0" data-toggle="counter-up">18</h1>
                     </div>
                 </div>
-                <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.7s">
+                <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.5s">
                     <div class="fact-item bg-light rounded text-center h-100 p-5">
                         <i class="fa fa-check fa-4x text-primary mb-4"></i>
-                        <h5 class="mb-3">Projects Done</h5>
-                        <h1 class="display-5 mb-0" data-toggle="counter-up">1234</h1>
+                        <h5 class="mb-3">Projetos Realizados</h5>
+                        <h1 class="display-5 mb-0" data-toggle="counter-up">57</h1>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-    <!-- Facts End -->
+    <!-- Informacoes -->
 
 
     <!-- About Start -->
