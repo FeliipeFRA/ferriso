a0ca2d83d335b8ca1407072cca85f6dae15b313c
Felipe Ferreira <165099343+FeliipeFRA@users.noreply.github.com>
2025-10-30 11:37:57 -0300

    TENTANDO ARRUMAR O SEO

--- admin/partials/nav.php
+++ admin/partials/nav.php
@@ -14,6 +14,7 @@
     <link rel="stylesheet" href="adminlte/dist/css/adminlte.min.css">
     <!-- Favicon -->
     <link href="../../img/favicon.ico" rel="icon">
+    
 </head>
 
 <body class="hold-transition sidebar-mini">
